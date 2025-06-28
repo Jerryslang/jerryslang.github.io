b708424a2454edce56a6982ae07d04153837090a
Noface <166717111+Jerryslang@users.noreply.github.com>
2025-06-28 06:15:26 +0100

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,41 @@
+setCategories(['Git', 'Pastebin']);
+
+addProject({
+  name: 'MCLaunch Changelog',
+  url: 'https://pastebin.com/KMnAiFyT',
+  description: 'Changelog for MClaunch',
+  language: 'txt',
+  category: 'Pastebin'
+});
+
+addProject({
+  name: 'Dotfiles',
+  url: 'https://github.com/Jerryslang/dotfiles',
+  description: 'my sway wm dotfiles',
+  language: 'Shell & CSS',
+  category: 'Git'
+});
+
+addProject({
+  name: 'MClaunch',
+  url: 'https://github.com/Jerryslang/MClaunch',
+  description: 'Minecraft Java Launcher / Downloader (without authentication) ',
+  language: 'Python',
+  category: 'Git'
+});
+
+addProject({
+  name: 'JrpcPy',
+  url: 'https://github.com/Jerryslang/jrpcPy',
+  description: 'a python libary for interacting with the xbox 360 jrpc2 and xbdm libary over lan ',
+  language: 'Python',
+  category: 'Git'
+});
+
+addProject({
+  name: 'BF To X86ASM Transpiler',
+  url: 'https://github.com/Jerryslang/BF-to-ASM-transpiler',
+  description: "a transpiler that also uses nasm to create x86_64 asm binary's from brainfuck code",
+  language: 'Python, Brainfuck, Assembly, Makefile',
+  category: 'Git'
+});
